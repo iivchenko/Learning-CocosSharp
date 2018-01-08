4cb4252a0c86f9f7233c8818dbe78e82ece1501e
Ivan Ivcheno <ivan.ivchenko@citrix.om>
2018-01-08 22:02:34 +0200

    Add CCAction Demo sample

---
+++ src/CocosSharpSamples/CCAction/CocosSharpSamples.CCAction.UWP/GamePage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using CocosSharp;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CocosSharpSamples.CCAction.UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class GamePage
+    {
+        public GamePage(LaunchActivatedEventArgs args)
+        {
+            this.InitializeComponent();
+
+            DisplayInformation.AutoRotationPreferences = DisplayOrientations.Landscape;
+
+            CCApplication.Create(new GameDelegate(), args, Window.Current.CoreWindow, this);
+        }
+    }
+}
